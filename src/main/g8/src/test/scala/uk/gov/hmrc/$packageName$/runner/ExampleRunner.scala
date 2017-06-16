d546c22307b9e87116c246cdae148a48d6fad809
stephen.kam <stephen.kam@digital.hmrc.gov.uk>
2017-06-16 15:08:29 +0100

    Paramerterised the package name in the runner

--- src/main/g8/src/test/scala/uk/gov/hmrc/$packageName$/runner/ExampleRunner.scala
+++ src/main/g8/src/test/scala/uk/gov/hmrc/$packageName$/runner/ExampleRunner.scala
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith
 @RunWith(classOf[Cucumber])
 @CucumberOptions(
 features = Array("src/test/resources/features"),
-glue = Array("uk.gov.hmrc.scubasteve.stepdefs"),
+glue = Array("uk.gov.hmrc.$packageName$.stepdefs"),
 format = Array ("pretty", "html:target/cucumber", "json:target/cucumber.json"),
 tags = Array("@RunOnlyInDev")
 )
