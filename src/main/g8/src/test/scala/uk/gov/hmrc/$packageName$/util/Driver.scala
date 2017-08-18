5b0dadce89c86e4fe12ebe75367cce2f49d000c8
stephen.kam <stephen.kam@digital.hmrc.gov.uk>
2017-08-18 14:26:56 +0100

    modified singtondriver to stop it from opening a new instance for each scenario

--- src/main/g8/src/test/scala/uk/gov/hmrc/$packageName$/util/Driver.scala
+++ src/main/g8/src/test/scala/uk/gov/hmrc/$packageName$/util/Driver.scala
@@ -2,94 +2,93 @@ package uk.gov.hmrc.$packageName$.util
 
 import java.util.concurrent.TimeUnit
 
-import cats.syntax.either._
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
-import org.openqa.selenium.firefox.{FirefoxDriver, MarionetteDriver}
+import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.remote.{BrowserType, DesiredCapabilities}
 
 object Driver {
 
   private val systemProperties = System.getProperties
-
-  def newWebDriver(): Either[String, WebDriver] = {
-    val selectedDriver: Either[String, WebDriver] = Option(systemProperties.getProperty("browser")).map(_.toLowerCase) match {
-      case Some("firefox") ⇒ Right(createFirefoxDriver())
-      case Some("chrome") ⇒ Right(createChromeDriver())
-      case Some("gecko") ⇒ Right(createGeckoDriver())
-      case Some(other) ⇒ Left(s"Unrecognised browser: \$other")
-      case None ⇒ Left("No browser set")
-    }
-
-    selectedDriver.map { driver ⇒
-      sys.addShutdownHook(driver.quit())
-      driver.manage().window().maximize()
-      driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS)
-    }
-    selectedDriver
-  }
-
-  private val os: String =
-    Option(systemProperties.getProperty("os.name")).getOrElse(sys.error("Could not read OS name"))
-
+  private val os: String = Option(systemProperties.getProperty("os.name")).getOrElse(sys.error("Could not read OS name"))
   private val isMac: Boolean = os.startsWith("Mac")
-
   private val isLinux: Boolean = os.startsWith("Linux")
-
-  private val linuxArch: String =
-    Option(systemProperties.getProperty("os.arch")).getOrElse(sys.error("Could not read OS arch"))
-
+  private val linuxArch: String = Option(systemProperties.getProperty("os.arch")).getOrElse(sys.error("Could not read OS arch"))
   private val isJsEnabled: Boolean = true
-
   private val driverDirectory: String = "drivers"
+  private var baseWindowHandle: String = _
 
-  private def createGeckoDriver(): WebDriver = {
-    if (isMac) {
-      systemProperties.setProperty("webdriver.gecko.driver", driverDirectory + "/geckodriver_mac")
-    } else if (isLinux) {
-      systemProperties.setProperty("webdriver.gecko.driver", driverDirectory + "/geckodriver_linux64")
-    } else {
-      systemProperties.setProperty("webdriver.gecko.driver", driverDirectory + "/geckodriver.exe")
+  var instance: WebDriver = _
+
+  def getInstance(): WebDriver = {
+    if (instance == null) {
+      initialiseBrowser()
+    }
+    instance
+  }
+
+  private def createBrowser(): WebDriver = {
+    def createChromeDriver(): WebDriver = {
+      if (isMac) {
+        systemProperties.setProperty("webdriver.chrome.driver", driverDirectory + "/chromedriver_mac")
+      } else if (isLinux && linuxArch == "amd32") {
+        systemProperties.setProperty("webdriver.chrome.driver", driverDirectory + "/chromedriver_linux32")
+      } else if (isLinux) {
+        systemProperties.setProperty("webdriver.chrome.driver", driverDirectory + "/chromedriver")
+      } else {
+        systemProperties.setProperty("webdriver.chrome.driver", driverDirectory + "/chromedriver.exe")
+      }
+
+      val capabilities = DesiredCapabilities.chrome()
+      val options = new ChromeOptions()
+
+      options.addArguments("test-type")
+      options.addArguments("--disable-gpu")
+
+      capabilities.setJavascriptEnabled(isJsEnabled)
+      capabilities.setCapability(ChromeOptions.CAPABILITY, options)
+
+      val driver = new ChromeDriver(capabilities)
+      driver
     }
 
-    val capabilities = DesiredCapabilities.firefox()
-    capabilities.setJavascriptEnabled(isJsEnabled)
+    def createFirefoxDriver(): WebDriver = {
+      val capabilities = DesiredCapabilities.firefox()
+      capabilities.setJavascriptEnabled(true)
+      capabilities.setBrowserName(BrowserType.FIREFOX)
 
-    new MarionetteDriver(capabilities)
-  }
-
-  private def createFirefoxDriver(): WebDriver = {
-    val capabilities = DesiredCapabilities.firefox()
-    capabilities.setJavascriptEnabled(true)
-    capabilities.setBrowserName(BrowserType.FIREFOX)
-
-    new FirefoxDriver(capabilities)
-  }
-
-  private def createChromeDriver(): WebDriver = {
-    if (isMac) {
-      systemProperties.setProperty("webdriver.chrome.driver", driverDirectory + "/chromedriver_mac")
-    } else if (isLinux && linuxArch == "amd32") {
-      systemProperties.setProperty("webdriver.chrome.driver", driverDirectory + "/chromedriver_linux32")
-    } else if (isLinux) {
-      systemProperties.setProperty("webdriver.chrome.driver", driverDirectory + "/chromedriver")
-    } else {
-      systemProperties.setProperty("webdriver.chrome.driver", driverDirectory + "/chromedriver.exe")
+      new FirefoxDriver(capabilities)
     }
 
-    val capabilities = DesiredCapabilities.chrome()
-    val options = new ChromeOptions()
+    def createGeckoDriver(): WebDriver = {
+      if (isMac) {
+        systemProperties.setProperty("webdriver.gecko.driver", driverDirectory + "/geckodriver_mac")
+      } else if (isLinux) {
+        systemProperties.setProperty("webdriver.gecko.driver", driverDirectory + "/geckodriver")
+      } else {
+        systemProperties.setProperty("webdriver.gecko.driver", driverDirectory + "/geckodriver.exe")
+      }
 
-    options.addArguments("test-type")
-    options.addArguments("--disable-gpu")
+      val capabilities = DesiredCapabilities.firefox()
+      capabilities.setCapability("marionette", true)
 
-    capabilities.setJavascriptEnabled(isJsEnabled)
-    capabilities.setCapability(ChromeOptions.CAPABILITY, options)
+      new FirefoxDriver(capabilities)
+    }
 
-    new ChromeDriver(capabilities)
+    val environmentProperty = System.getProperty("browser")
+    environmentProperty match {
+      case "firefox" ⇒ createFirefoxDriver()
+      case "chrome" ⇒ createChromeDriver()
+      case "gecko" ⇒ createGeckoDriver()
+      case _ => throw new IllegalArgumentException(s"Browser type not recognised: -D\$environmentProperty")
+    }
   }
 
-
-
+  def initialiseBrowser() {
+    instance = createBrowser()
+    instance.manage().window().maximize()
+    //    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
+    baseWindowHandle = instance.getWindowHandle
+  }
 
 }
